d9f572583bc4ab8e9398c1f4a1f2221e18ba3f7d
Adrian Rocamora <adrianrocamorav@gmail.com>
2020-03-14 18:59:25 -0600

    Data wrangling and LaTeX test

--- data_exploration.py
+++ data_exploration.py
@@ -54,14 +54,17 @@ df_confirmed = pd.read_csv(os.path.join(data_dir, file_confimed))
 df_deaths = pd.read_csv(os.path.join(data_dir, file_deaths))
 df_recovered = pd.read_csv(os.path.join(data_dir, file_recovered))
 
+# Sort by highest value at most recently updated date
+df_confirmed.sort_values(by='3/10/20', inplace=True)
+df_deaths.sort_values(by='3/10/20', inplace=True)
+df_recovered.sort_values(by='3/10/20', inplace=True)
+
 # + 
-df_confirmed
+df_deaths
 
 # + [markdown]
 # ## Analyzing a famous cruise ship's data with this dataset
-# The Diamond Princess cruise ship is maybe one of the most famous (and 
-# experimentally controlled) sources of information for studying the contagion
-# dynamics and death distribution of the virus.
+# The Diamond Princess cruise ship is maybe one of the most famous (and experimentally controlled) sources of information for studying the contagion dynamics and death distribution of the virus.
 #
 # Let's first estimate the death percentage of confirmed cases for this ship
 
@@ -70,5 +73,15 @@ province_state = 'Diamond Princess cruise ship'
 dp_deaths = df_deaths['3/10/20'][df_deaths['Province/State'] == province_state]
 dp_confirmed = df_confirmed['3/10/20'][df_confirmed['Province/State'] == province_state]
 
+# + [markdown]
+# Also let's plot out the distribution of deaths per zone
+print(df_deaths)
+df_confirmed.plot(x='Country/Region', y='3/10/20', style='o')
+df_deaths.plot(x='Country/Region', y='3/10/20', style='o')
+plt.show()
+
 # + 
 print('Death percentage of confirmed cases', str(round(dp_deaths.iloc[0]/dp_confirmed.iloc[0]*100, 2))+'%')
+
+# + [markdown]
+# Now that we've tested the dataset a bit we are ready to plot out other things as well as try to model the disease. One possible equation we can use is the classic SIR compartmental model with components: susceptible $S(t)$, infected $I(t)$, and removed $R(t)$
